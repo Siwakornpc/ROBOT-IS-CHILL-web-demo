99ef6afa93e8109d56a189aff16f9f42042ff10d
htest-311429731023102 <jburgers60@gmail.com>
2026-08-15 00:51:43 +0800

    separate filters panel

--- components/page/search/Body.tsx
+++ components/page/search/Body.tsx
@@ -3,18 +3,17 @@ import SearchTags from "./SearchButtons";
 
 export default function Body() {
     return (
-        <div className="filter-controls">
-            <p className="text-label">Search</p>
-            <SearchTags/>
-        </div>
         <main style={{ width: "stretch" }}>
             <div className="main-body">
+                <div className="filter-controls">
+                    <p className="text-label">Search</p>
+                    <SearchTags/>
+                </div>
                 <input
                     className="searchbar"
                     contentEditable="true"
                     placeholder="search for something..."
                 />
-                <p className="text-label">---</p>
                 <hr />
                 <RecieveScreen/>
             </div>
