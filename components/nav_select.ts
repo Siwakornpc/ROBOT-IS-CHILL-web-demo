6b8984a469150bbdd2dd0471147b13c832c01e66
htest-311429731023102 <jburgers60@gmail.com>
2026-08-14 22:15:25 +0800

    tooltips

--- components/nav_select.ts
+++ components/nav_select.ts
@@ -1,7 +1,5 @@
 export function nav_btn_select(name: string) {
-    const nav_btn = document.querySelector(`.nav-btn[aria-label='{name}']`);
+    const nav_btn = document.querySelector(`.nav-btn[aria-label='${name}']`);
 
     nav_btn?.classList.add("selected");
 }
-
-
--- components/page/SideBars.tsx
+++ components/page/SideBars.tsx
@@ -1,14 +1,14 @@
 export function LeftBar() {
     return (
         <div className="navrail">
-            <div className="nav-btn">
-                <a href="../macrosia" className="icon custom" aria-label="macrosia">macrosia</a>
+            <div className="nav-btn" aria-label="macrosia">
+                <a href="../macrosia" className="icon custom">macrosia</a>
             </div>
-            <div className="nav-btn">
-                <a href="../render" className="icon custom" aria-label="render">render</a>
+            <div className="nav-btn" aria-label="render">
+                <a href="../render" className="icon custom">render</a>
             </div>
-            <div className="nav-btn">
-                <a href="../macrosia" className="icon" aria-label="search">search</a>
+            <div className="nav-btn" aria-label="search">
+                <a href="../macrosia" className="icon">search</a>
             </div>
         </div>
     );
