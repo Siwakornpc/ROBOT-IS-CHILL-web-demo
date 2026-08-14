8377c0dfcaf58e187893ff42826214c6dd4f02d6
htest-311429731023102 <jburgers60@gmail.com>
2026-08-14 16:57:14 +0800

    scrolling classes

--- components/render-screen/macrosia/RenderScreen.tsx
+++ components/render-screen/macrosia/RenderScreen.tsx
@@ -1,13 +1,7 @@
 export function RenderScreen() {
     return (
-        <div className="render-screen">
-<<<<<<< HEAD
-            <div className="ascroll-y" id="render-output"></div>
-=======
-            <div className="render-screen-scroll">
-                <div id="render-output"></div>
-            </div>
->>>>>>> 297e595ed6166122ad37551d6fc4d322db9254bd
+        <div className="render-screen ascroll-y">
+            <div id="render-output"></div>
         </div>
     );
 }
