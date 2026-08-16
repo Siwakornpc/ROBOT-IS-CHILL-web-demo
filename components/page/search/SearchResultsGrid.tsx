2601cabc932f151bbaa6c6d754e65a71caba08e0
htest-311429731023102 <jburgers60@gmail.com>
2026-08-16 12:49:15 +0800

    nvm

--- components/page/search/SearchResultsGrid.tsx
+++ components/page/search/SearchResultsGrid.tsx
@@ -3,11 +3,7 @@ export default function SearchResults() {
         <div className="search-results">
             {Array.from({ length: 24 }, (_, index) => (
                 <div className="search-item" key={index}>
-                    <div className="search-item-tile">
-                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_1.png?raw=true"/>
-                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_2.png?raw=true"/>
-                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_3.png?raw=true"/>
-                    </div>
+                    <img className="search-item-tile" src="https://ric-api.sno.mba/tiles/debug.gif"/>
                     <span className="search-item-name">debug</span>
                 </div>
             ))}
