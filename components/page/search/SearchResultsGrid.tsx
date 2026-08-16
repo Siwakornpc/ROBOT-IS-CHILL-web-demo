7057006e77f6a16804069428e0bb39323fc13fbb
htest-311429731023102 <jburgers60@gmail.com>
2026-08-16 12:26:31 +0800

    tile animation thing

--- components/page/search/SearchResultsGrid.tsx
+++ components/page/search/SearchResultsGrid.tsx
@@ -3,7 +3,11 @@ export default function SearchResults() {
         <div className="search-results">
             {Array.from({ length: 24 }, (_, index) => (
                 <div className="search-item" key={index}>
-                    <img className="search-item-tile" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_1.png?raw=true"/>
+                    <div className="search-item-tile">
+                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_1.png?raw=true"/>
+                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_2.png?raw=true"/>
+                        <img className="search-item-tile-frame" src="https://github.com/ROBOT-IS-CHILL/robot-is-chill/blob/main/data/sprites/balt-sprites/debug_0_3.png?raw=true"/>
+                    </div>
                     <span className="search-item-name">debug</span>
                 </div>
             ))}
