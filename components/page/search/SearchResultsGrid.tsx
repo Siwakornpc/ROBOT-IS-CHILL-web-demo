726a60d02cdb92fc47e58285727a5e3405b6996f
htest-311429731023102 <jburgers60@gmail.com>
2026-08-16 12:51:32 +0800

    ascroll class

--- components/page/search/SearchResultsGrid.tsx
+++ components/page/search/SearchResultsGrid.tsx
@@ -1,6 +1,6 @@
 export default function SearchResults() {
     return (
-        <div className="search-results">
+        <div className="search-results ascroll-y">
             {Array.from({ length: 24 }, (_, index) => (
                 <div className="search-item" key={index}>
                     <img className="search-item-tile" src="https://ric-api.sno.mba/tiles/debug.gif"/>
