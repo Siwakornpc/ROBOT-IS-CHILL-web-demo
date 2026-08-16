e8f94af40704d795e32e728213fa3ec6db636a74
htest-311429731023102 <jburgers60@gmail.com>
2026-08-16 17:13:48 +0800

    some rtl layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@ export function Header() {
     return (
         <div className="topbar">
             <a href="/" className="title-name">
-                <span id="name-1">ROBOT IS CHILL </span>
+                <span id="name-1">ROBOT IS CHILL</span>
+                &nbsp;
                 <span id="name-2">web demo</span>
             </a>
         </div>
