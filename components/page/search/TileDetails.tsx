f9d76cb8ee7cd87ca15fa5a165602e30f56e3de5
htest-311429731023102 <jburgers60@gmail.com>
2026-08-20 12:16:23 +0800

    apparently you cant just % css variables

--- components/page/search/TileDetails.tsx
+++ components/page/search/TileDetails.tsx
@@ -37,6 +37,46 @@ export function Details({ selected }: DetailsProps) {
             })
             .join("");
     }
+    
+    const TILINGS = {
+        none: [0],
+        directional: [0, 8, 16, 24],
+        tiling: [
+            [ 0,  1,  5,  4],
+            [ 8,  9, 13, 12],
+            [10, 11, 15, 14],
+            [ 2,  3,  7,  6],
+        ],
+        diagonal_tiling: [
+            [ 0,  1,  5,  4, 23, 36, 27, 19, 25, 40, 43, ""],
+            [ 8,  9, 13, 12, 35, 44, 45, 28, 34, 42, 26, 39],
+            [10, 11, 15, 14, 18, 41, 33, 22, 38, 46, 31, 30],
+            [ 2,  3,  7,  6, 29, 17, 21, 37, 16, 24, 20, 32],
+        ],
+        character: [
+            [ 0,  1,  2,  3,  7],
+            [ 8,  9, 10, 11, 15],
+            [16, 17, 18, 19, 23],
+            [24, 25, 26, 27, 31],
+        ]
+    } as const;
+
+    type TilingName = keyof typeof TILINGS;
+
+    function mapTiling(
+        name: string,
+        tiling: TilingName | TilingName[]
+    ): string[] | string[][] {
+        if (Array.isArray(tiling)) {
+            return tiling.map((item) => mapTiling(name, item)) as string[] | string[][];
+        }
+
+        const mapped = TILINGS[tiling];
+
+        return mapped.map(
+            (map) => `https://ric-api.sno.mba/tiles/${name}.gif${map !== 0 ? `?frame=${map}` : ""}`
+        );
+    }
 
     if ("tile" in selected) {
         return (
