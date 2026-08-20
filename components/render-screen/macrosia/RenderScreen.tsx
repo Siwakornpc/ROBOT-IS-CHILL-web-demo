a34ea7cdcced249414248fb4f4b5663392456666
Siwakornpc <proarm2009@gmail.com>
2026-08-20 10:04:28 +0700

    Update RenderScreen.tsx

--- components/render-screen/macrosia/RenderScreen.tsx
+++ components/render-screen/macrosia/RenderScreen.tsx
@@ -1,9 +1,7 @@
 export function RenderScreen() {
     return (
-        <div className="render-screen">
-            <div className="render-screen-scroll">
-                <div id="render-output"></div>
-            </div>
+        <div className="render-screen ascroll-y">
+            <div id="render-output"></div>
         </div>
     );
 }
