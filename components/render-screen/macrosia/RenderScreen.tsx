b2432876f4f4147144ce5488d0e37cd4cba715be
Siwakornpc <proarm2009@gmail.com>
2026-08-21 08:26:55 +0700

    fix render-screen/output-screen to scroll + fixed text center + scroll problem

--- components/render-screen/macrosia/RenderScreen.tsx
+++ components/render-screen/macrosia/RenderScreen.tsx
@@ -1,6 +1,11 @@
+import { applyOverflowFade } from "@/components/OverflowFade";
+
 export function RenderScreen() {
     return (
-        <div className="render-screen ascroll-y">
+        <div
+            ref={(el) => applyOverflowFade(el, "y")}
+            className="render-screen ascroll-y"
+        >
             <div id="render-output"></div>
         </div>
     );
