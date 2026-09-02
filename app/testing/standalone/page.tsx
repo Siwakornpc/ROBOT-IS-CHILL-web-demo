995ad99158f090d95cee1fa3051399e7be40806c
Siwakornpc <proarm2009@gmail.com>
2026-09-02 20:27:25 +0700

    make thing not automatucally stretch (flex)

--- app/testing/standalone/page.tsx
+++ app/testing/standalone/page.tsx
@@ -1,27 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import MenuSelect from "@/components/MenuSelect";
 
 export default function TestingPage() {
-    const [username, setUsername] = useState(null);
-
-    async function test() {
-        const response = await fetch(
-            "/api/username?id=842018963248513074"
-        );
-
-        const data = await response.json();
-
-        setUsername(data.username);
-    }
-
-    test();
-
     return (
-        <div>
-            {username && (
-                <p>Username: {username}</p>
-            )}
-        </div>
-    );
+        <MenuSelect
+            id=""
+            title="this:key"
+            value=""
+            options={[{value:"",label:""}]}
+            onChange={() => []}
+        />
+    )
 }
