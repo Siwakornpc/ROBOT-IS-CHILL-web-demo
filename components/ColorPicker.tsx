854ec65aa79ffbcf67f19e149d934013ade76f88
Siwakornpc <proarm2009@gmail.com>
2026-09-02 20:15:29 +0700

    hell yeah color picker

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -391,7 +391,7 @@ export default function ColorPicker({
                 hexInputRef.current.value = newColor;
             }
 
-            pendingColorRef.current = newColor;
+            onChange(newColor);
         });
     };
 
