764afa61cc231a072d5c38b540e0aa13e5066622
htest-311429731023102 <jburgers60@gmail.com>
2026-09-07 21:30:25 +0800

    remove overflowfade from renderscreen

--- components/render-screen/render/RenderScreen.tsx
+++ components/render-screen/render/RenderScreen.tsx
@@ -2,11 +2,8 @@ import applyOverflowFade from "@/components/OverflowFade";
 
 export function RenderScreen() {
     return (
-        <div
-            ref={(el) => applyOverflowFade(el, "y")}
-            className="render-screen ascroll-y"
-        >
-            <div id="render-output"></div>
+        <div className="render-screen ascroll-y">
+            <div id="render-output" />
         </div>
     );
 }
