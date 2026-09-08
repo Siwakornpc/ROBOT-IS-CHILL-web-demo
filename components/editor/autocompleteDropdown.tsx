791087140c796dd0e79379225495ca9162e8a6d8
htest-311429731023102 <jburgers60@gmail.com>
2026-09-08 17:28:31 +0800

    username instead

--- components/editor/autocompleteDropdown.tsx
+++ components/editor/autocompleteDropdown.tsx
@@ -23,7 +23,7 @@ interface AutocompleteProps {
 function MacroCreator({ id }: { id: string }) {
     const { user, loading } = useDiscordUser(id);
 
-    return <p>@{loading ? "loading..." : user?.display_name ?? id}</p>;
+    return <p>@{loading ? "loading..." : user?.username ?? id}</p>;
 }
 
 export function AutocompleteDropdown({
