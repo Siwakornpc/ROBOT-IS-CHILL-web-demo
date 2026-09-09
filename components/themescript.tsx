4349a868b6737a8c10e460a60602a11719a69c73
htest-311429731023102 <jburgers60@gmail.com>
2026-09-09 16:37:31 +0800

    c

--- components/themescript.tsx
+++ components/themescript.tsx
@@ -7,6 +7,7 @@ const customThemeColors = {
     success: '#84cc7b',
     syntaxName: '#72a5e7',
     syntaxValue: '#ffcb22',
+    syntaxEscaped: '#ff8147',
     syntaxBracketLayer0: { color: '#f1c43e', blend: false }, 
     syntaxBracketLayer1: { color: '#c85acc', blend: false }, 
     syntaxBracketLayer2: { color: '#5f94f5', blend: false },
